2e3d55357ce136f67da14ebed6a3e6ffe370384d
jjditaranto <jjditaranto@loyola.edu>
2021-04-14 12:59:36 -0400

    continued working on part 2 and have our dataset loaded into an array and some other stuff done, plan to use fit_transform from scikit label encoder

--- DataMove.py
+++ DataMove.py
@@ -2,7 +2,20 @@ import numpy as np
 
 
 def main():
-    data = np.loadtxt("CS484 Dataset.csv", skiprows=1, dtype=)
-    print(data.shape)
+    dataStrings = np.loadtxt("CS484CleanDataset.csv", skiprows=1, dtype=str, delimiter=',')
+   # # data = np.loadtxt("CS484CleanDataset.csv", skiprows=1, dtype={'names': ('Symboling', 'Make', 'Fuel-Type',
+   #                                                                          'Aspiration', '# of Doors', 'Body-Style',
+   #                                                                          'Drive-Wheels', 'Engine-Location',
+   #                                                                          'Wheel-Base',
+   #                                                                          'Length', 'Width', 'Height', 'Curb-Weight',
+   #                                                                          'Engine Type', '# of Cylinders',
+   #                                                                          'Engine Size',
+   #                                                                          'Fuel System', 'Bore', 'Stroke',
+   #                                                                          'Compression Ratio', 'Horsepower',
+   #                                                                          'Peak RPM', 'City MPG',
+   #                                                                          'Highway MPG', 'Price'),
+   #                                                                'formats': ('i4', 'S1', '')})
+    print(dataStrings.shape)
+    print(dataStrings)
 
 main()
